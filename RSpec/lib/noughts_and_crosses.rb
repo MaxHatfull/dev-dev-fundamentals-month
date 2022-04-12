b72405702fbbb565a1f8d8b2c05eae82aa5dcad2
Max Hatfull <max.hatfull@freeagent.com>
2022-04-12 17:01:26 +0100

    Make tic tac toe

--- RSpec/lib/noughts_and_crosses.rb
+++ RSpec/lib/noughts_and_crosses.rb
@@ -1,8 +1,48 @@
 # frozen_string_literal: true
 
+class InvalidPositionError < StandardError; end
+
 class NoughtsAndCrosses
-  # This is merely a suggestion. You can change the API in any way you like!
-  def play_game
-    raise StandardError, "Implement me"
+  def self.start_game
+    NoughtsAndCrosses.new
+  end
+
+  def initialize
+    @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @current_player = :o
+  end
+
+  attr_reader :board
+
+  def take_turn(position)
+    raise InvalidPositionError if @board[position].is_a? Symbol
+
+    @board[position] = @current_player
+    @current_player = opponent
+  end
+
+  def winner
+    [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+    ].map do |winning_combination|
+      if winning_combination.map { |i| @board[i] }.uniq.length == 1
+        @board[winning_combination[0]]
+      end
+    end.compact.fetch(0, nil)
+  end
+
+  private
+
+  def opponent
+    @current_player == :o ? :x : :o
   end
 end
--- RSpec/spec/noughts_and_crosses_spec.rb
+++ RSpec/spec/noughts_and_crosses_spec.rb
@@ -3,11 +3,68 @@
 require "noughts_and_crosses"
 
 RSpec.describe NoughtsAndCrosses do
-  subject(:game) { described_class.new }
+  it "starts a new game" do
+    expect(described_class.start_game.class).to eq(described_class)
+  end
 
-  describe "#play_game" do
-    it "runs the game" do
-      expect(game.play_game).to eq(true)
+  context "when a game is running" do
+    let!(:game) { described_class.start_game }
+
+    it "returns the board" do
+      expect(game.board).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    end
+
+    describe "#take_turn" do
+      subject { game.board }
+
+      context "when the first turn has been taken" do
+        before { game.take_turn(4) }
+
+        it { is_expected.to eq [0, 1, 2, 3, :o, 5, 6, 7, 8] }
+      end
+
+      it "allows the second turn to be taken" do
+        game.take_turn(4)
+        game.take_turn(5)
+
+        expect(game.board).to eq([0, 1, 2, 3, :o, :x, 6, 7, 8])
+      end
+
+      it "does not allow you to take a turn in an occupied position" do
+        game.take_turn(4)
+
+        expect { game.take_turn(4) }.to raise_error(InvalidPositionError)
+      end
+    end
+
+    describe "#winner" do
+      it "has the correct winner when one player gets 3 in a row" do
+        game.take_turn(0)
+        game.take_turn(5)
+        game.take_turn(1)
+        game.take_turn(8)
+        game.take_turn(2)
+
+        expect(game.winner).to eq :o
+      end
+
+      it "has the correct winner when one player gets 3 in a column" do
+        game.take_turn(5)
+        game.take_turn(0)
+        game.take_turn(8)
+        game.take_turn(3)
+        game.take_turn(4)
+        game.take_turn(6)
+
+        expect(game.winner).to eq :x
+      end
+
+      it "is nil when there is no winner" do
+        game.take_turn(0)
+        game.take_turn(5)
+
+        expect(game.winner).to eq nil
+      end
     end
   end
 end
