7ad5e95e0ee2550d87b03b9ab0e220a4e80303f1
Max Hatfull <max.hatfull@freeagent.com>
2022-04-19 09:58:44 +0100

    Refactoring

--- lib/noughts_and_crosses.rb
+++ lib/noughts_and_crosses.rb
@@ -9,7 +9,8 @@ class NoughtsAndCrosses
 
   def initialize
     @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    @current_player = :o
+    @players = [:o, :x]
+    @current_player_index = 0
   end
 
   attr_reader :board
@@ -17,24 +18,13 @@ class NoughtsAndCrosses
   def take_turn(position)
     raise InvalidPositionError if @board[position].is_a? Symbol
 
-    @board[position] = @current_player
-    @current_player = opponent
+    @board[position] = players[current_player_index]
+    @current_player_index = (current_player_index + 1) % players.length
   end
 
   def winner
-    [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-
-      [0, 4, 8],
-      [2, 4, 6],
-    ].map do |winning_combination|
-      if winning_combination.map { |i| @board[i] }.uniq.length == 1
+    winning_combinations.map do |winning_combination|
+      if winning_combination.map { |position| @board[position] }.uniq.length == 1
         @board[winning_combination[0]]
       end
     end.compact.fetch(0, nil)
@@ -42,7 +32,26 @@ class NoughtsAndCrosses
 
   private
 
-  def opponent
-    @current_player == :o ? :x : :o
+  attr_reader :current_player_index
+  attr_reader :players
+
+  def winning_combinations
+    [2,7,6,9,5,1,4,3,8]
+      .combination(3)
+      .select {|combination| combination.sum == 15 }
+
+
+    # [
+    #   [0, 1, 2],
+    #   [3, 4, 5],
+    #   [6, 7, 8],
+
+    #   [0, 3, 6],
+    #   [1, 4, 7],
+    #   [2, 5, 8],
+
+    #   [0, 4, 8],
+    #   [2, 4, 6],
+    # ]
   end
 end
