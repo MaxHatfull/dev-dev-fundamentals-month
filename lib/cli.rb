ac814ce4701c1a4cb79993e51fa579c14efc13e3
Max Hatfull <max.hatfull@freeagent.com>
2022-04-19 12:28:41 +0100

    Interface layer

---
+++ lib/cli.rb
@@ -0,0 +1,37 @@
+require_relative './noughts_and_crosses.rb'
+
+class Cli
+  def self.start_new_game(input = $stdin, output = $stdout)
+    @@current_game = Cli.new(input, output)
+  end
+
+  private
+
+  def initialize(input = $stdin, output = $stdout)
+    @input = input
+    @output = output
+    @game = NoughtsAndCrosses.new
+    game_loop
+  end
+
+  def game_loop
+    display_board
+    instruction = @input.gets
+    return if instruction == 'exit'
+
+    desired_position = Integer(instruction)
+    @game.take_turn(desired_position)
+
+    if @game.winner
+      display_board
+      @output.puts "Congrats #{@game.winner}"
+    else
+      game_loop
+    end
+  end
+
+  def display_board
+    @output.puts @game.board.each_slice(3).map { |s| s.join(' | ') }.join("\n- + - + -\n")
+    @output.puts
+  end
+end
